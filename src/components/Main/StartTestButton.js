b18fafcbb07ce3a6d7c0cab7d57599e84e8ecaae
kk217 <kkc217@naver.com>
2022-05-06 18:02:44 +0900

    feat: add submit button component

---
+++ src/components/Main/StartTestButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StartTestButton = () => {
+    return (
+        <>
+            <input type="submit" value="테스트 시작"/>
+        </>
+    )
+}
+
+export default StartTestButton;
