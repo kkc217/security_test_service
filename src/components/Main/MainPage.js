0a6a4d7994cd73075e8b1dcbb5be9e17e75040be
kk217 <kkc217@naver.com>
2022-05-07 19:36:03 +0900

    update: apply style sheet

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -2,37 +2,32 @@ import React from 'react';
 import RadioButton from '../Constants/RadioButton';
 import SelectBox from '../Constants/SelectBox';
 import StartTestButton from './StartTestButton';
-
-const Colors = [
-    { name: 'RED', hex: '#ffb598' },
-    { name: 'ORANGE', hex: '#ffdcaa' },
-    { name: 'PURPLE', hex: '#d7beff' },
-    { name: 'CYAN', hex: '#c7f5ed' },
-    { name: 'BLUE', hex: '#c2dbff' },
-];
+import '../../assets/css/Main/mainPage.css';
 
 const MainPage = () => {
     return (
-        <>  
-            <div className="radio_container">
-                점검 유형
-                <RadioButton name="test_selection" items={ ['파일 업로드', 'XSS', 'SQL Injection'] } checked="파일 업로드"/>
+        <div className="main_page_container">
+            <div className="main_page_items">
+                <div className="radio_container">
+                    <span className="selection_name">Test Type</span>
+                    <RadioButton name="test_selection" items={ ['Upload File', 'XSS', 'SQL Injection'] } checked="Upload File"/>
+                </div>
+                <input id="url_input" type="text" name="url_selection" placeholder="Enter the URL"></input>
+                <div className="radio_container">
+                    <span className="selection_name">Option1</span>
+                    <RadioButton name="option1_selection" items={ ['Option 1-1', 'Option 1-2', 'Option 1-3'] } checked="Option 1-1"/>
+                </div>
+                <div className="radio_container">
+                    <span className="selection_name">Option2</span>
+                    <RadioButton name="option2_selection" items={ ['Option 2-1', 'Option 2-2'] } checked="Option 2-1"/>
+                </div>
+                <div className="select_box_container">
+                    <span className="selection_name">Option3</span>
+                    <SelectBox name="option3_selection" items={ ['Option 3-1', 'Option 3-2', 'Option 3-3', 'Option 3-4'] }/>
+                </div>
+                <StartTestButton/>
             </div>
-            <input type="text" name="url_selection" placeholder="URL을 입력해주세요."></input>
-            <div className="radio_container">
-                속성1
-                <RadioButton name="option1_selection" items={ ['속성 1-1', '속성 1-2', '속성 1-3'] } checked="속성 1-1"/>
-            </div>
-            <div className="radio_container">
-                속성2
-                <RadioButton name="option2_selection" items={ ['속성 2-1', '속성 2-2'] } checked="속성 2-1"/>
-            </div>
-            <div className="select_box_container">
-                속성3
-                <SelectBox name="option3_selection" items={ ['속성 3-1', '속성 3-2', '속성 3-3', '속성 3-4'] }/>
-            </div>
-            <StartTestButton/>
-        </>
+        </div>
     )
 }
 
