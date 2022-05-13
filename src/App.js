4b838449d32af7bde167651ba503a7099c5161e9
kk217 <kkc217@naver.com>
2022-05-13 20:11:07 +0900

    feat: add navigation bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './components/Main/MainPage';
 // import logo from './logo.svg';
 // import './App.css';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavigationBar/>
         <Routes>
           <Route exact path='/' element={<MainPage/>} />
         </Routes>
