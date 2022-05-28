d5df034edef717a7d595ed14df2d9461b48b89b4
kk217 <kkc217@naver.com>
2022-05-29 00:59:08 +0900

    feat: add explanation page component

--- src/components/Intro/IntroPage.js
+++ src/components/Intro/IntroPage.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+
+import Explanation from "./Explanation";
 
 import "../../assets/css/Intro/introPage.css";
 
@@ -9,13 +8,7 @@ const IntroPage = () => {
     return (
         <>
             <div className="intro_page_container">
-                <div className="description_container">
-                    Descriptions of our page.
-                </div>
-                <div className="test_description_container">
-                    Test descriptions.
-                </div>
-                <Link to='/main' className="test_link">Go Test <FontAwesomeIcon icon={faArrowRightLong} /></Link>
+                <Explanation />
             </div>
         </>
     )
